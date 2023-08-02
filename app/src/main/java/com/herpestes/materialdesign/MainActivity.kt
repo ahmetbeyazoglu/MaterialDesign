f9913d666bc7cdb1080d341abc39a5a56d665ecb
herpestes <ahmetbeyazoglu@gmail.com>
2023-08-02 22:38:54 +0300

    MaterialDesign

---
+++ app/src/main/java/com/herpestes/materialdesign/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.herpestes.materialdesign
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.herpestes.materialdesign.ui.theme.MaterialDesignTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialDesignTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    SayfaTopAppBar()
+                }
+            }
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MaterialDesignTheme {
+        SayfaTopAppBar()
+    }
+}
+
+
+
+@Composable
+fun SayfaTopAppBar() {
+    Scaffold(
+        topBar = {
+                 TopAppBar(
+                     title = {
+                        Column() {
+                            Text(text = "Başlık")
+                            Text(text = "Alt Başlık", fontSize = 13.sp)
+
+                        }
+                     }
+
+                     )
+
+        },
+        content = {
+            
+        }
+
+    )
+
+
+}
+
