48a42f5a58ee95559a3848ad34780905d11c394a
herpestes <ahmetbeyazoglu@gmail.com>
2023-08-02 22:42:32 +0300

    Action

--- app/src/main/java/com/herpestes/materialdesign/MainActivity.kt
+++ app/src/main/java/com/herpestes/materialdesign/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.herpestes.materialdesign
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,8 +52,12 @@ fun SayfaTopAppBar() {
                         Column() {
                             Text(text = "Başlık")
                             Text(text = "Alt Başlık", fontSize = 13.sp)
-
                         }
+                     },
+                     backgroundColor = colorResource(id = R.color.anarenk),
+                     contentColor = Color.White,
+                     actions = {
+                         Text(text = "Çıkış", modifier = Modifier.clickable { Log.e("TopBar", "Çıkış seçildi") })
                      }
 
                      )
