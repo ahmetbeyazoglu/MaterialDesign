25f98bc07637e1de92f5a5f906114562ca4d661d
herpestes <ahmetbeyazoglu@gmail.com>
2023-08-03 22:17:30 +0300

    LazyColumn

--- app/src/main/java/com/herpestes/materialdesign/MainActivity.kt
+++ app/src/main/java/com/herpestes/materialdesign/MainActivity.kt
@@ -4,20 +4,23 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.herpestes.materialdesign.ui.theme.MaterialDesignTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,19 +33,72 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    SayfaTopAppBarArama()
+                    SayfaSabitListeleme()
                 }
             }
         }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MaterialDesignTheme {
-        SayfaTopAppBarArama()
+        SayfaSabitListeleme()
     }
 }
+
+@Composable
+fun SayfaSabitListeleme() {
+    LazyColumn {
+        item {
+            Card(
+                modifier = Modifier
+                    .padding(all = 5.dp)
+                    .fillMaxWidth()
+            ) {
+                Row(modifier = Modifier.clickable {
+                    Log.e("Liste", "Güneş tıklandı")
+                }) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(all = 10.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.gunes_resim),
+                            contentDescription = ""
+                        )
+                        Text(text = "Güneş", modifier = Modifier.padding(all = 5.dp))
+                    }
+                }
+            }
+        }
+        item {
+            Card(
+                modifier = Modifier
+                    .padding(all = 5.dp)
+                    .fillMaxWidth()
+            ) {
+                Row(modifier = Modifier.clickable {
+                    Log.e("Liste", "Ay tıklandı")
+                }) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(all = 10.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ay_resim),
+                            contentDescription = ""
+                        )
+                        Text(text = "Ay", modifier = Modifier.padding(all = 5.dp))
+                    }
+                }
+            }
+        }
+    }
+
+}
+/*
 @Composable
 fun SayfaTopAppBarArama() {
     val aramayapiliyormu = remember { mutableStateOf(false) }
@@ -155,5 +211,5 @@ fun SayfaTopAppBar() {
     )
 
 
-}
+}*/
 
